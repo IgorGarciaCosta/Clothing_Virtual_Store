e56c0b227125488c1bca568cee355a9d1be748c6
IgorGarciaCosta <igonildo7@gmail.com>
2020-11-06 10:36:53 -0300

    grid view dos itens

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,8 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_three/datas/genre_data.dart';
+import 'package:loja_three/tiles/genre_tile.dart';
 
 class CategoryScreen extends StatelessWidget {
-
   final DocumentSnapshot snapshot;
 
   CategoryScreen(this.snapshot);
@@ -12,25 +13,65 @@ class CategoryScreen extends StatelessWidget {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        appBar: AppBar(
-          title: Text(snapshot.data["title"]),
-          centerTitle: true,
-          bottom: TabBar(
-            indicatorColor: Colors.white,//barrinha qued indica em que ícone está
-            tabs: <Widget>[
-              Tab(icon: Icon(Icons.grid_on),),
-              Tab(icon: Icon(Icons.list),)
-            ],
+          appBar: AppBar(
+            title: Text(snapshot.data["title"]),
+            centerTitle: true,
+            bottom: TabBar(
+              indicatorColor:
+                  Colors.white, //barrinha qued indica em que ícone está
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.grid_on),
+                ),
+                Tab(
+                  icon: Icon(Icons.list),
+                )
+              ],
+            ),
           ),
-        ),
-        body: TabBarView(
-          physics: NeverScrollableScrollPhysics(),//impede de mudar entre frid e lista deslizando pro lado
-          //aqui dentro ficam o grid e a lista
-          children: [
-            Container(color: Colors.red,),
-            Container(color: Colors.green,)
-          ],
-        ),
+          body: FutureBuilder<QuerySnapshot>(
+            future: Firestore.instance.collection("generos").document(snapshot.documentID)
+              .collection("items").getDocuments(),//pega todos os gêneros lá do banco
+            builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              //se não retornar nada
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            else {
+              return TabBarView(
+                physics:
+                    NeverScrollableScrollPhysics(), //impede de mudar entre frid e lista deslizando pro lado
+                //aqui dentro ficam o grid e a lista
+                children: [
+                  //GRID
+                  GridView.builder(//não carrega tudo de vez
+                      padding: EdgeInsets.all(4.0),//espaço em todos os lados
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 4.0,
+                          crossAxisSpacing: 4.0,
+                          childAspectRatio: 0.65),//núm de itens na horizintal
+                      itemCount: snapshot.data.documents.length,
+                      itemBuilder: (context, index){
+                        return GenreTile("grid", GenreData.fromDocument(snapshot.data.documents[index]));
+                      }),
+
+
+                  //LISTA
+                  ListView.builder(
+                     padding: EdgeInsets.all(4.0),
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (context, index){
+                      return GenreTile("list", GenreData.fromDocument(snapshot.data.documents[index]));
+                    }
+                  )
+                ],
+              );
+            }
+          }
+          )
       ),
     );
   }
